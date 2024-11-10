ac05d0dd0baf9a02fa9b7bf3330e60c7df7f6d25
mahendrareddyduggireddy <mahendrareddyduggireddyin@gmail.com>
2024-11-10 14:53:40 +0530

    lock-unlock-app

---
+++ src/appindex.js
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+
+const MyApp=()=>{
+const [currentState,setCreds]=useState({statusText:"Your Device is Locked",
+    btnText:"Unlock",
+    imgUrl:"https://assets.ccbp.in/frontend/hooks/lock-img.png",
+    altText:"unlock"})
+
+    const onUnlock=()=>{
+      setCreds({statusText:"Your Device is Unlocked",
+        btnText:"Lock",
+        imgUrl:"https://assets.ccbp.in/frontend/hooks/unlock-img.png",
+        altText:"lock"})
+    }
+    
+    const onLock=()=>{
+     setCreds({statusText:"Your Device is Locked",
+        btnText:"Unlock",
+        imgUrl:"https://assets.ccbp.in/frontend/hooks/lock-img.png",
+        altText:"unlock"})
+    }
+
+    const {statusText,btnText,altText,imgUrl}=currentState
+    
+return <div>
+    <img src={imgUrl} alt={altText}/>
+    <h1>{statusText}</h1>
+    {btnText==="Unlock"?<button type="button" onClick={onUnlock}>UnLock</button>:<button type="button" onClick={onLock}>Lock</button>}
+</div>
+}
+export default MyApp

f74fe9e4c6cb5278ee4c5a52586a631029da26f8
mahendrareddyduggireddy <mahendrareddyduggireddyin@gmail.com>
2024-11-10 20:29:27 +0530

    lock-unlock-app

--- src/appindex.js
+++ src/appindex.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import './appindex.css'
 
 const MyApp=()=>{
 const [currentState,setCreds]=useState({statusText:"Your Device is Locked",
@@ -22,7 +23,7 @@ const [currentState,setCreds]=useState({statusText:"Your Device is Locked",
 
     const {statusText,btnText,altText,imgUrl}=currentState
     
-return <div>
+return <div className="container">
     <img src={imgUrl} alt={altText}/>
     <h1>{statusText}</h1>
     {btnText==="Unlock"?<button type="button" onClick={onUnlock}>UnLock</button>:<button type="button" onClick={onLock}>Lock</button>}
